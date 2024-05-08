23ec4e16db7abd2faecc198a3f39841730135a87
nikvrv <oliukov@gmail.com>
2024-05-08 23:03:48 +0400

    change tests

--- conftest.py
+++ conftest.py
@@ -2,12 +2,19 @@ import pytest
 import requests
 
 from helpers import create_store_name
-from config import URL
 import json
+
+import os
+
+
+@pytest.fixture(scope='session')
+def url():
+    app_url = os.getenv('APP_URL', 'http://127.0.0.1:9000')
+    return app_url
 
 
 @pytest.fixture
-def create_store():
+def create_store(url):
     name = create_store_name()
-    response = requests.post(f"{URL}/stores", data=json.dumps({"name": name}))
+    response = requests.post(f"{url}/stores", data=json.dumps({"name": name}))
     return response.json()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,6 @@
 import json
 
 import pytest
-
-from config import URL
 import requests
 from helpers import create_store_name,  create_item_data
 
@@ -11,23 +9,23 @@ class TestItem:
     ENDPOINT = 'items'
 
     @pytest.fixture()
-    def create_item(self, create_store):
+    def create_item(self, url, create_store):
         data = create_item_data(create_store['id'])
-        response = requests.post(f"{URL}/{self.ENDPOINT}", data=json.dumps(data))
+        response = requests.post(f"{url}/{self.ENDPOINT}", data=json.dumps(data))
         return response.json()
 
-    def test_create_item(self, create_store):
+    def test_create_item(self, url, create_store):
         store_id = create_store['id']
         data = create_item_data(store_id)
-        response = requests.post(f"{URL}/{self.ENDPOINT}", data=json.dumps(data))
+        response = requests.post(f"{url}/{self.ENDPOINT}", data=json.dumps(data))
         assert response.status_code == 201
 
-    def test_get_item(self, create_item):
-        response = requests.get(f"{URL}/{self.ENDPOINT}/{create_item['id']}")
+    def test_get_item(self, url, create_item):
+        response = requests.get(f"{url}/{self.ENDPOINT}/{create_item['id']}")
         assert response.status_code == 200
 
-    def test_delete_item(self, create_item):
-        response = requests.delete(f"{URL}/{self.ENDPOINT}/{create_item['id']}")
+    def test_delete_item(self, url, create_item):
+        response = requests.delete(f"{url}/{self.ENDPOINT}/{create_item['id']}")
         assert response.status_code == 200
 
 
@@ -35,20 +33,20 @@ class TestItem:
 class TestStore:
     ENDPOINT = 'stores'
 
-    def test_create_store(self):
+    def test_create_store(self, url):
         name = create_store_name()
-        response = requests.post(f"{URL}/{self.ENDPOINT}", data=json.dumps({"name": name}))
+        response = requests.post(f"{url}/{self.ENDPOINT}", data=json.dumps({"name": name}))
         assert response.status_code == 201
         print(response.text)
 
-    def test_get_store(self, create_store):
+    def test_get_store(self, create_store, url):
         store_id = create_store['id']
-        response = requests.get(f"{URL}/{self.ENDPOINT}/{store_id}")
+        response = requests.get(f"{url}/{self.ENDPOINT}/{store_id}")
         assert response.status_code == 200
         print(response.text)
 
-    def test_delete_store(self, create_store):
+    def test_delete_store(self, create_store, url):
         store_id = create_store['id']
-        response = requests.delete(f"{URL}/{self.ENDPOINT}/{store_id}")
+        response = requests.delete(f"{url}/{self.ENDPOINT}/{store_id}")
         assert response.status_code == 200
         print(response.text)
